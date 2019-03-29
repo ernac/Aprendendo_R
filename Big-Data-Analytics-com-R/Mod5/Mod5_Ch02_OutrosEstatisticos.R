1ac9d80e86f0de8b8237b6f1790331312260299e
ernac <37631725+ernac@users.noreply.github.com>
2019-03-28 21:18:55 -0300

    Alteração Estrutural

---
+++ Big-Data-Analytics-com-R/Mod5/Mod5_Ch02_OutrosEstatisticos.R
@@ -0,0 +1,8 @@
+# Importação de dados de outros sw estatísticos
+
+# R, SAS, SPSS, Matlab, Scipy, NumPy, Matplotlib
+# Fontes variadas de análises com R
+# Uso concomitante de outras ferramentas com R
+# Cada uma com funções e momentos diferentes
+
+
